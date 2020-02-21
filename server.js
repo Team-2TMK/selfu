3c48976415e3aeca5eac4c24b7ee7e7d5dc2a09e
Tenaw_Derseh <mete439@gmail.com>
2020-02-20 21:30:38 -0800

    required

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 'use strict';
+
+// require
 require('dotenv').config();
 const express = require('express');
-//const pg = require('pg');
+const pg = require('pg');
+const superagent = required('supergaent');
+const cors = required('cors');
 const app = express();
 
-app.use(express.static(__dirname+'/public'));
+//  EJS
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+
 
 app.get('/', (request,response) =>{
   response.send('Hello World!');
