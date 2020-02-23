f32726d0345bacf99dc9bf7bf7a7eb7e48a4c405
Madison Stehle <mnstehle3@gmail.com>
2020-02-23 14:19:20 -0800

    quiz questions record values

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,12 @@
 'use strict';
 
-$('#positive').on('click', function() {
-  console.log('clicked');
-});
+let x = 0;
 
-$('#submit').on('click', function() {
+// $('#positive').on('click', function() {
+//   console.log('clicked');
+// });
+
+$('#finalSubmit').on('click', function() {
   let url = 'http://localhost:3000/newresult';
   console.log(url);
   $.ajax(url, {
@@ -16,5 +18,30 @@ $('#submit').on('click', function() {
     });
 });
 
-console.log('hello world');
+// console.log('hello world');
 
+// $('input[name="name_of_your_radiobutton"]:checked').val();
+
+// ======= QUESTION 1 =======
+$('#one .enter').on('click', function(event) {
+  event.preventDefault();
+  if ( $('input[name="questionOne"]:checked').val() === 'positive' ) {
+    x++;
+    console.log(x);
+  } else if ( $('input[name="questionOne"]:checked').val() === 'negative' ) {
+    x--;
+    console.log(x);
+  }
+});
+
+// ======= QUESTION 2 ========
+$('#two .enter').on('click', function(event) {
+  event.preventDefault();
+  if ( $('input[name="questionTwo"]:checked').val() === 'positive' ) {
+    x++;
+    console.log(x);
+  } else if ( $('input[name="questionTwo"]:checked').val() === 'negative' ) {
+    x--;
+    console.log(x);
+  }
+});
